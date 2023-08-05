111372461aa67e86456a5a07678c30e22f31b64f
mlgxmez <mlgxmez@gmail.com>
2023-08-05 21:10:44 +0200

    Update definition of exp2 and test in/out types

--- test_funcs.py
+++ test_funcs.py
@@ -2,5 +2,5 @@ from wheeldemoado.funcs import exp2, exp3
 
 
 def test_funcs():
-    assert exp2(5) == 32
-    assert exp3(3) == 27
+    assert exp2(5.0) == 32.0
+    assert exp3(3.0) == 27.0
--- wheeldemoado/funcs.py
+++ wheeldemoado/funcs.py
@@ -2,7 +2,7 @@ import math
 
 
 def exp2(x: float) -> float:
-    return math.exp2(x)
+    return 2**x
 
 
 def exp3(x: float) -> float:
